c388cfad737e812f4b6454fecfa6661ba85009fa
ayoutsey <youtsey2@illinois.edu>
2019-12-08 13:51:06 -0600

    Finance1.1

---
+++ app/src/main/java/com/example/personalfinance/CreateAccount.java
@@ -0,0 +1,42 @@
+package com.example.personalfinance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccount extends AppCompatActivity {
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        Button addAccount = findViewById(R.id.addAccount);
+        addAccount.setVisibility(View.VISIBLE);
+        addAccount.setOnClickListener(unchecked -> addAccount());
+        Button cancelAccount = findViewById(R.id.cancelAccount);
+        cancelAccount.setVisibility(View.VISIBLE);
+        cancelAccount.setOnClickListener(unchecked -> cancelAccount());
+    }
+
+    public void addAccount() {
+        EditText accountName = findViewById(R.id.accountName);
+        EditText accountBalance = findViewById(R.id.accountBalance);
+        String name = accountName.getText().toString();
+        String bal = accountBalance.getText().toString();
+        double balance = Double.parseDouble(bal);
+        Account newAccount = new Account(name, balance);
+        LaunchActivity.addAccount(newAccount);
+        Intent intent = new Intent(this, LaunchActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void cancelAccount() {
+        Intent intent = new Intent(this, LaunchActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/personalfinance/LaunchActivity.java
+++ app/src/main/java/com/example/personalfinance/LaunchActivity.java
@@ -1,5 +1,37 @@
 package com.example.personalfinance;
 
-public class LaunchActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+public class LaunchActivity extends AppCompatActivity {
+
+    private static ArrayList<Account> accountList;
+
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        Button createAccount = findViewById(R.id.createAccount);
+        createAccount.setVisibility(View.VISIBLE);
+        createAccount.setOnClickListener(unchecked -> createAccount());
+        accountList = new ArrayList<Account>();
+    }
+
+    public void createAccount() {
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public static void addAccount(Account a) {
+        accountList.add(a);
+    }
 }
