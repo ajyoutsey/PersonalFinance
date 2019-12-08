3c585c03d9835f9c69cd7caab2ab7171af6ae302
jkang74 <jkang74@illinois.edu>
2019-12-08 17:59:50 -0600

    chunks

--- app/src/main/java/com/example/personalfinance/CreateEvent.java
+++ app/src/main/java/com/example/personalfinance/CreateEvent.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,11 +25,22 @@ public class CreateEvent extends AppCompatActivity {
         EditText eventName = findViewById(R.id.eventName2);
         EditText eventValue = findViewById(R.id.eventCost);
         EditText eventDate = findViewById(R.id.eventDate);
+        Switch eventSign = findViewById(R.id.eventSign);
         String eName = eventName.getText().toString();
         double eValue = Double.parseDouble(eventValue.getText().toString());
-        int eDate = Integer.parseInt(eventDate.getText().toString());
+        String tempEDate = eventDate.getText().toString();
+        String[] forInt = tempEDate.split("/");
+        String toConvert = "";
+        if (!eventSign.getTextOn().toString().equals("Positive")) {
+            toConvert += "-";
+        }
+        for (int i = 0; i < forInt.length; i++) {
+            toConvert += forInt[i];
+        }
+        int eDate = Integer.parseInt(toConvert);
         Account temp = LaunchActivity.getAccountList().get(LaunchActivity.getCurrentAccount());
-        temp.addEvent(eValue, eName, eDate);
+        Event tempE = new Event(eValue, eName, eDate);
+        temp.addEvent(tempE);
         Intent intent = new Intent(this, AccountPage.class);
         startActivity(intent);
         finish();
